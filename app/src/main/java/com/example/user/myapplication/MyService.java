9914ff51716706ac3f257470342eac82c26cc2e0
yeowok <5087pj@gmail.com>
2018-11-22 14:56:11 +0800

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/MyService.java
@@ -0,0 +1,41 @@
+package com.example.user.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    public void onCreate() {
+        new Thread(){
+            boolean flag=true;
+            int count=0;
+            @Override
+            public void run(){
+                while(flag){
+
+                    Intent i=new Intent("MyMessage");
+                    i.putExtra("background_service",count);
+                    sendBroadcast(i);
+                    count++;
+                    try{
+                        Thread.sleep(1000);
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }.start();
+
+    }
+}
